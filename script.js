65afc918503110b1035c48e3e4655196ce8974f7
EwuraAnnie <ewuranhyira03@gmail.com>
2022-03-11 15:53:55 +0000

    message

---
+++ script.js
@@ -0,0 +1,46 @@
+//How to declare a variable
+// var - variable
+// let - let(scope)
+// const - constant
+let firstName = "John";
+let lastName = "Doe";
+let age = 21;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+//String Concatenation
+console.log("My name is " + firstName + " " + lastName + ". I am " + age + " " + "years old");
+
+//String Intepolation
+console.log(`My name is ${firstName} ${lastName}. I am ${age} years old`);
+
+//How to declare an array
+let fruits = ["Mango", "Orange", "Pineapple", "Apple", "Grapes", "Peach"]
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits[2]);
+console.log(fruits.push("Banana"));
+console.log(fruits);
+console.log(fruits.pop());
+console.log(fruits.pop());
+console.log(fruits);
+console.log(fruits.unshift("Pear"));
+console.log(fruits);
+console.log(fruits.shift());
+console.log(fruits);
+console.log(fruits.includes("Pineapple"));
+console.log(fruits);
+console.log(fruits.includes("Guava"));
+console.log(fruits.join("-"));
+
+
+
+// let userAge = [12, 21, 22]
+// console.log(userAge);
+
+
+//How to declare an object
+// let person = {userFirstName: "Miss", userLastName: "Annan"}
+// console.log(person);
